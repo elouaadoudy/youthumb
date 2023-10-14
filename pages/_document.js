820062a75b12be6e915a3ec9254ac4d68416fa01
elouaadoudy <ismailelw5@gmail.com>
2023-10-14 21:08:59 +0100

    ading adsense

--- pages/_document.js
+++ pages/_document.js
@@ -30,33 +30,29 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <div style={{ textAlign: 'center', padding: '10px', backgroundColor: 'lightgray' }}>
-            <p>YouTube Thumbnail Downloader
-▷ Save YT Video Thumbnails
-Get free thumbnail image of any youtube videos in Full HD(1080), HD (720), SD, and also in small size. it's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos
+            <p><h1>YouTube Thumbnail Downloader</h1>
+    <p>▷ Save YT Video Thumbnails</p>
+    <p>Get free thumbnail image of any YouTube videos in Full HD(1080), HD (720), SD, and also in small size. It's currently supported formats: YouTube (HD, HQ, 1080p, 4K) videos</p>
 
-What is the use of  YouTube Thumbnail Grabber website?
-People use  YouTube thumbnail downloader website for getting thumbnail from any youtube videos. That can be used in presentation, animation work or many other activities.
+    <h2>What is the use of  YouTube Thumbnail Grabber website?</h2>
+    <p>People use  YouTube thumbnail downloader website for getting thumbnails from any YouTube videos. That can be used in presentations, animation work, or many other activities.</p>
 
-How to use  YouTube thumbnail downloader website?
-I have shared some of screenshot in which i have guided you how you can use  YouTube downloader website. It's quite easy first step is you just copy any YouTube link whom thumbnail you want. youtube cover download Easily you can download you tube cover
+    <h2>How to use  YouTube Thumbnail Downloader website?</h2>
+    <p>I have shared some screenshots in which I have guided you on how you can use this YouTube downloader website. It's quite easy. The first step is to copy any YouTube link whose thumbnail you want. Easily, you can download YouTube covers.</p>
+    <p>Now paste that URL in the input box, the moment you paste that link, it's automatically going to generate different sizes of thumbnails for you. Now click on the thumbnail download button, and it's going to be automatically downloaded to your system. If you are using an Android phone, then you have to save those images, and if you are using an iPhone, then I don't know how you can do it.</p>
 
-Now paste that url in input box, the moment you paste that link it's automatically going to generate different size of thumbnails for you. Now click on thumbnail download button and it's going to automatically downloaded in your system. If you are using Android phone then you have to saved that images and if you are using iphone then i don't know how you can do it?
+    <h2>Is it legal to download YouTube thumbnails?</h2>
+    <p>Of course, it's 100% legal to download any YouTube video's thumbnails. But as you know, thumbnails and videos are copyrighted products, so you should take author permissions first in order to reuse them.</p>
 
-Is it legal to download YouTube thumbnails?
-Of course it's 100% legal you can download any YouTube videos thumbnails but as you know thumbnail and video are copyrighted product you should take author permissions 1st in order to reuse that.
+    <h2>Compatibility of  YT Thumbnail Grabber website</h2>
+    <p>This YouTube thumbnail downloader website is going to work well on all devices except iPhone because iPhone doesn't allow images to be saved. But if you are using a jailbroken iPhone, then there is no problem. On the other hand, it's going to work fine on almost all kinds of Android devices and laptop or desktop systems.</p>
 
-Compatibility of  YT thumbnail grabber website
-This YouTube thumbnail downloader website is going to work well in all devices except iPhone, because iPhone doesn't allow images to be saved in iPhone. But if you using jailbroken iphone then there is no any problem. on the other hand is going to work fine in almost all kinds Android devices and laptop or desktop system.
+    <h2>Is there any Copyright risk on YouTube Thumbnails?</h2>
+    <p>Whatever YouTube screenshot you are downloading is a copyright of the respective owner of that video. If you want to use it in your work, then you should ask for permission.</p>
+    <p>Especially if you are reusing it on YouTube, then it can create problems. But if your plan is outside of YouTube, like using it on a website for creating logos or any Photoshop types of stuff, then there is no problem. You will hardly receive any DMCA complaints related to that particular YouTube thumbnail.</p>
 
-Is there any Copyright risk on YouTube Thumbnails?
-Whatever youtube screenshot you are downloading a copyright of respective on owner of that videos. If you want to use it in your work then you should ask for permission.
-
-Especially if you are reusing on YouTube then it's can create problem but if your plan is outside of YouTube. like use on website for creating logos or any Photoshop types of stuff then there is no any problem. You hardly receive any dmca complaint about related to that particular Youtube thumnail.
-
-Is reusing YouTube thumbnails are SEO friendly?
-Nope it's not, in short answer it's not seo friendly because almost all thumbnails which are on YouTube are indexed by Google. So which one is unique and which one is duplicate google know it. So it's not SEO friendly as you know. But you can do some stuff on that thumbnail I mean getting some unique effects using photoshop or any other software that can make your thumbnail unique again so that can be reused and make that seo friendly again.
-
-youtube video thumbnail download</p>
+    <h2>Is reusing YouTube thumbnails SEO friendly?</h2>
+    <p>No, it's not. In short, it's not SEO friendly because almost all thumbnails on YouTube are indexed by Google. So which one is unique and which one is duplicate, Google knows it. So it's not SEO friendly as you know. But you can do some stuff on that thumbnail, I mean getting some unique effects using Photoshop or any other software that can make your thumbnail unique again, so it can be reused and made SEO friendly again.</p></p>
           </div>
         </body>
       </Html>
