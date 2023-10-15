f4e74c2e2982afe13e2f2362ceeb88e116401936
elouaadoudy <ismailelw5@gmail.com>
2023-10-15 18:59:59 +0100

    ading adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,16 @@
 import "../styles/index.css";
 import { Fragment, useEffect } from "react";
 import { DefaultSeo } from "next-seo";
-import initializeGTM from '../utils/gtm'; // Import initializeGTM function
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga'; // Import react-ga
 
 function MyApp({ Component, pageProps }) {
+  // Initialize Google Analytics
   useEffect(() => {
-    // Initialize Google Analytics
-    ReactGA.initialize('G-P54R920DWK'); // Replace with your Google Analytics tracking ID
-    initializeGTM(); // Initialize Google Tag Manager
+    ReactGA.initialize('G-P54R920DWK'); // Replace 'YOUR_TRACKING_ID' with your actual tracking ID
   }, []);
 
+  // Track page views when the route changes
   useEffect(() => {
-    // Track page views when the route changes
     ReactGA.pageview(window.location.pathname);
   }, [Component]);
 
